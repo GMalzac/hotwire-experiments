28fe07592500813eaf96ee4f15a1e1c382d448b9
Gregoire Malzac <gregoire.malzac@gmail.com>
2021-05-09 16:11:33 +0100

    Set default values for post in migration

--- db/migrate/20210509150336_create_posts.rb
+++ db/migrate/20210509150336_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :username
+      t.string :username, default: 'Greg'
       t.text :body
-      t.integer :likes_count
-      t.integer :repost_count
+      t.integer :likes_count, default: 0
+      t.integer :repost_count, default: 0
 
       t.timestamps
     end
