28e6447ec9e246df4394298969472abec260ed19
Gregoire Malzac <gregoire.malzac@gmail.com>
2021-05-09 16:24:40 +0100

    Add routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts, only: %i[index create] do
+    get 'like'
+    get 'repost'
+  end
+  
+  root to: 'posts#index'
 end
